b8401257de294a5fbcc9a9ddeec45a46bfba695d
jullianaa <julliana.coutoalmeida@gmail.com>
2017-06-06 13:13:19 -0300

    Iniciando desenvolvimento de rendimento empresarial utilizando MVC

---
+++ MVC/src/Controller/AntigoRendimento.java
@@ -0,0 +1,9 @@
+package Controller;
+import model.Rendimento;
+
+public class AntigoRendimento {
+
+	public void calculaRendimento(String nomeProduto){
+		System.out.println("Nome do produto: " + nomeProduto);
+	}
+}
